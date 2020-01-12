e96b48f9f6a2ba8d3706ef02ab443ad9266f73b0
Joshua Le Gresley <joshlegresley@hotmail.com>
2020-01-12 23:23:13 +1100

    medium screen change complee

--- dist/js/media.js
+++ dist/js/media.js
@@ -1,22 +1,8 @@
 function myFunction(x) {
     if (x.matches) { // If media query matches
-        $('.home').addClass('fluid-container');
-        $('.home').addClass('pb-5');
-        $('.home').addClass('px-4');
-        $('.home div').addClass('row');
-        $('.home div div').addClass('col-md');
-        $('.home div div h2').addClass('line');
-        $('.home div div h2').addClass('typing2');
-        $('.home div div h2').addClass('home_name');
+        $('.home').replaceWith('<section class="home fluid-container pb-5 px-4"><div class="row"><div class="col-md"><h1 id="l1" class="h3 line typing1 home__name">Joshua Le Gresley</h1></div><div class="col-md"><h2 id="l2" class="h4 line typing2 home__name">Software Developer</h2></div></div></section>');
     } else {
-        $('.home').removeClass('fluid-container');
-        $('.home').removeClass('pb-5');
-        $('.home').removeClass('px-4');
-        $('.home div').removeClass('row');
-        $('.home div div').removeClass('col-md');
-        $('.home div div h2').removeClass('line');
-        $('.home div div h2').removeClass('typing2');
-        $('.home div div h2').removeClass('home_name');
+        $('.home').replaceWith('<section class="home"><div class=""><h1 class="home__name line typing">Joshua Le Gresley</h1></div><div class=""><h2>Web developer</h2></div></section>');
     }
 }
 
