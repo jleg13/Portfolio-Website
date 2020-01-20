fad0098a40992fe8aa133f47e33ab6062bc8a922
Joshua Le Gresley <joshlegresley@hotmail.com>
2020-01-20 22:04:43 +1100

    complete responsiveness

---
+++ dist/js/projectLabels.js
@@ -0,0 +1,25 @@
+$('#carouselExampleCaptions').on('slide.bs.carousel', function (e) {
+    removeCurrentLabel()
+    if (e.relatedTarget.id === 'project1') {
+        $("#project_label1").addClass('activeLabel');
+    } else if (e.relatedTarget.id === 'project2') {
+        $("#project_label2").addClass('activeLabel');
+    } else if (e.relatedTarget.id === 'project3') {
+        $("#project_label3").addClass('activeLabel');
+    } else if (e.relatedTarget.id === 'project4') {
+        $("#project_label4").addClass('activeLabel');
+    } else if (e.relatedTarget.id === 'project5') {
+        $("#project_label5").addClass('activeLabel');
+    }
+
+});
+
+function removeCurrentLabel() {
+    //remove current active label
+    $('.projects__sm__Labels').each(function () {
+        if ($(this).hasClass("activeLabel")) {
+
+            $(this).removeClass("activeLabel");
+        }
+    });
+}
